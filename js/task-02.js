0c79ed7798ba92983a933a1e5481766290f778fa
lilyafunny <125149545+lilyafunny@users.noreply.github.com>
2023-09-16 11:29:21 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulEl = document.querySelector("#ingredients");
+
+function add(arr) {
+  const markup = arr.map((text) => {
+    const liEl = document.createElement("li");
+    liEl.textContent = text;
+    liEl.classList.add("item");
+
+    ulEl.append(liEl);
+  });
+}
+
+add(ingredients);
