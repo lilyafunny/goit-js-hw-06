a74ceb8808997164780939da4198c47358019294
lilyafunny <125149545+lilyafunny@users.noreply.github.com>
2023-09-18 18:39:33 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector("form.login-form");
+console.log(form);
 
+form.addEventListener("submit", login);
+
+function login(event) {
+  event.preventDefault();
+  const inform = event.currentTarget;
+  const emailEL = inform.elements.email.value;
+  const passwordEl = inform.elements.password.value;
+
+  if (emailEL === "" || passwordEl === "") {
+    return alert("Please fill in all the fields!");
+  }
+  console.log(`Email: ${emailEL}, Password: ${passwordEl}`);
+  form.reset();
+}
