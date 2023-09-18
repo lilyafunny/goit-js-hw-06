04cda3cd050eb49eeff03168dd506296d1b1416f
lilyafunny <125149545+lilyafunny@users.noreply.github.com>
2023-09-18 21:18:34 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,27 @@
+const inputEl = document.querySelector("input");
+const buttonCreatEl = document.querySelector("button[data-create]");
+const buttonDestroyEl = document.querySelector("button[data-destroy]");
+const boxEl = document.querySelector("#boxes");
+
+inputEl.addEventListener("input", number);
+function number(event) {
+  return Number(event.currentTarget.value);
+}
+
+buttonCreatEl.addEventListener("click", createBoxes);
+
+function createBoxes(amount) {
+  for (let i = 0; i <= amount; i++) {
+    const divEl = document.createElement("div");
+    array.push(divEl);
+  }
+  boxEl.append(...array);
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+createBoxes(number);
