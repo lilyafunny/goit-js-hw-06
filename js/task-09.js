3b4ec5b26283269ec96f7790fc35d0bfea864b6d
lilyafunny <125149545+lilyafunny@users.noreply.github.com>
2023-09-18 19:34:45 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const body = document.body;
+const buttonEl = document.querySelector("button.change-color");
+const spanEl = document.querySelector(".color");
+
+buttonEl.addEventListener("click", changeColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function changeColor(event) {
+  body.style.background = getRandomHexColor();
+  spanEl.textContent = body.style.background;
 }
